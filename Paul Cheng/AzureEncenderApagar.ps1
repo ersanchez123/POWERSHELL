01e1eca63b5bec54437de1b77c18492bd84d119f
Eric Sanchez <ernosanchez@pancanal.com>
2025-09-11 11:26:38 -0500

    Subiendo carpeta de fuentes

--- Paul Cheng/AzureEncenderApagar.ps1
+++ Paul Cheng/AzureEncenderApagar.ps1
@@ -1,6 +1,13 @@
-﻿Import-Module AZ
-$credenciales = Get-Credential
-Connect-AzAccount -Credential $credenciales -ErrorAction Stop -Verbose
+﻿
+Write-Host "Iniciando sesión en Azure..." -ForegroundColor Yellow
+
+#$credenciales = Get-Credential
+#Connect-AzAccount -Credential $credenciales -ErrorAction Stop -Verbose
+
+Import-Module AZ
+$clientId = "edb7424f-a087-4b66-8aef-92b7acf22bec"
+Connect-AzAccount -Identity -AccountId $clientId -ErrorAction Stop
+
 Enable-AzureRmAlias
 
 #region Suscripciones
@@ -17,9 +24,12 @@ Get-AzVM -ResourceGroupName $resourcegroup -Status | Select-Object name,powersta
 
 # Encendido
 #Get-AzVM -ResourceGroupName $resourcegroup  | Start-AzVM  -NoWait 
+#Get-AzVM -ResourceGroupName "rg_group1" | Start-AzVM  -NoWait 
 
 # Apagado
 #Get-AzVM -ResourceGroupName $resourcegroup | Stop-AzVM -Force -NoWait
+
+#Get-AzVM -ResourceGroupName "rg_group1" | Stop-AzVM -Force -NoWait
 
 # Despues
 #Get-AzVM -ResourceGroupName $resourcegroup -Status | Select-Object name,powerstate
